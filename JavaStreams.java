10580ce50f568b3afdc5123ae3832f7653bf67f0
Teman Beck <temanbeck@yahoo.com>
2021-08-29 19:46:55 -0400

    Examples of implementing IntStream methods

--- JavaStreams.java
+++ JavaStreams.java
@@ -1,4 +1,6 @@
+import java.util.Arrays;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class JavaStreams {
     public static void main(String[] args) {
@@ -11,6 +13,35 @@ public class JavaStreams {
             System.out.println();
 
 
+        IntStream
+            .range(1,10)                            //range of 1-9
+            .skip(5)                                //skips first 5 elements of stream
+            .forEach(x -> System.out.println(x));   //lambda expression to print the elements pass in (x) basically parameter passed in
+            System.out.println();
+
+        System.out.println(                         //places inside println statement
+            IntStream                               
+            .range(1,5)                             //sum range 1-5, excluding 5 
+            .sum()                                  //adds elements in the range together
+        );
+
+        System.out.println();
+
+
+        //Stream.of, sorted and findFirst
+        Stream.of("Teman", "Nyheim", "Sceaira")
+            .sorted()                               //can sort, reverse sort, pass in own argument to search for
+            .findFirst()                            // find first item in list
+            .ifPresent(System.out::println);        //prints out the element found
+
+        System.out.println();
+
+        //Stream of Array, sort, filter and print
+        String[] names = {"Nylah", "Nyasia", "Teman", "Nya", "Sceaira", "Nyheim"};              //Array of names
+        Arrays.stream(names)                        //same as Stream.of(names)
+            .filter(x -> x.startsWith("N"))         //filters for names starting with N
+            .sorted()                               //sorts the filtered names
+            .forEach(System.out::println);          //prints to screen
 
         
     }
